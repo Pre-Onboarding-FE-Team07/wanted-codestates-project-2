5d010a9cb8e6cd642968582e489240f53cc0466d
jinyongp <dev.jinyongp@gmail.com>
2022-03-22 09:54:12 +0900

    Feat: 라우터 초기 설정

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/user/3">User</router-link> |
+    <router-link to="/rank">Rank</router-link> |
+    <router-link to="/track">Track</router-link>
   </nav>
   <router-view />
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,25 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
-    component: HomeView,
+    component: () => import(/* webpackChunkName: "home" */ '@/views/HomeView.vue'),
+  },
+  {
+    path: '/rank',
+    name: 'rank',
+    component: () => import(/* webpackChunkName: "rank" */ '@/views/RankView.vue'),
+  },
+  {
+    path: '/user/:id',
+    name: 'user',
+    component: () => import(/* webpackChunkName: "rank" */ '@/views/UserView.vue'),
+  },
+  {
+    path: '/track',
+    name: 'track',
+    component: () => import(/* webpackChunkName: "rank" */ '@/views/TrackView.vue'),
   },
 ];
 
