b1295a6df3bc81bcb0b472b2e14c8981dd1a7d0b
jinyongp <dev.jinyongp@gmail.com>
2022-03-22 09:59:01 +0900

    Chore: eslintrc 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,11 +4,13 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
   },
   rules: {
--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/user/3">User</router-link> |
-    <router-link to="/rank">Rank</router-link> |
-    <router-link to="/track">Track</router-link>
+    <router-link to="/">
+      Home
+    </router-link> |
+    <router-link to="/user/3">
+      User
+    </router-link> |
+    <router-link to="/rank">
+      Rank
+    </router-link> |
+    <router-link to="/track">
+      Track
+    </router-link>
   </nav>
   <router-view />
 </template>
