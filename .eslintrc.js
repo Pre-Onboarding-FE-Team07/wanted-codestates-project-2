f4446fc2c9a66fb2445fd8e197ce9d73c8e56e17
jinyongp <dev.jinyongp@gmail.com>
2022-03-22 10:20:58 +0900

    Chore: eslint-plugin-tailwindcss 설치

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,15 +4,19 @@ module.exports = {
     node: true,
   },
   extends: [
-    'plugin:vue/vue3-recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:tailwindcss/recommended',
   ],
   parser: 'vue-eslint-parser',
   parserOptions: {
     parser: '@typescript-eslint/parser',
     ecmaVersion: 2020,
   },
+  plugins: [
+    'tailwindcss',
+  ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
