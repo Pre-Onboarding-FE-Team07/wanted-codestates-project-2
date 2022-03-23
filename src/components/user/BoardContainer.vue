27af395c44e31d2af977ab31cf396398ba7a1b81
jinyongp <dev.jinyongp@gmail.com>
2022-03-23 16:50:45 +0900

    Refactor: BoardContainer 분리

---
+++ src/components/user/BoardContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="px-2 bg-white border border-gray-200">
+    <header class="flex items-center justify-between p-1 m-1 border-b">
+      <h5 class="my-1 font-bold">
+        <slot name="header" />
+        <!-- <span class="text-main">종합</span> 전적 -->
+      </h5>
+      <div class="flex items-center gap-1 text-xs">
+        <slot name="sub-header" />
+      </div>
+    </header>
+    <slot name="content" />
+  </div>
+</template>
--- src/components/user/TotalScoreBoard.vue
+++ src/components/user/TotalScoreBoard.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="px-2 bg-white border border-gray-200">
-    <header class="flex items-center justify-between p-1 m-1 border-b">
-      <h5 class="my-1 font-bold">
-        <span class="text-main">종합</span> 전적
-      </h5>
-      <div class="flex items-center gap-1 text-xs">
-        <span>200전</span>
-        <span>135승</span>
-        <span>65패</span>
+  <board-container>
+    <template #header>
+      <span class="text-main">종합</span> 전적
+    </template>
+    <template #sub-header>
+      <span>200전</span>
+      <span>135승</span>
+      <span>65패</span>
+    </template>
+    <template #content>
+      <div class="flex">
+        <circle-progress
+          v-for="(progress, index) in progressList"
+          :key="index"
+          :title="progress.title"
+          :value="progress.value"
+          :color="progress.color"
+          :stroke="7"
+          :radius="45"
+          class="px-2 border-r border-gray-200 last:border-r-0"
+        />
       </div>
-    </header>
-    <div class="flex">
-      <circle-progress
-        v-for="(progress, index) in progressList"
-        :key="index"
-        :title="progress.title"
-        :value="progress.value"
-        :color="progress.color"
-        :stroke="7"
-        :radius="45"
-        class="px-2 border-r border-gray-200 last:border-r-0"
-      />
-    </div>
-    <div class="flex items-center justify-between gap-1 p-2 mt-5 border-t">
-      <span class="text-sm text-main">최대주행</span>
-      <span class="text-sm">모드</span>
-      <h4 class="ml-auto text-lg font-bold">
-        통합
-      </h4>
-    </div>
-  </div>
+      <div class="flex items-center justify-between gap-1 p-2 mt-5 border-t">
+        <span class="text-sm text-main">최대주행</span>
+        <span class="text-sm">모드</span>
+        <h4 class="ml-auto text-lg font-bold">
+          통합
+        </h4>
+      </div>
+    </template>
+  </board-container>
 </template>
 
 <script lang="ts" setup>
 import CircleProgress from '@/components/shared/CircleProgress.vue';
+import BoardContainer from '@/components/user/BoardContainer.vue';
 
 const progressList = [{
   title: '승률',
