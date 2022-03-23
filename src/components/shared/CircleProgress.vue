efc2b4528cb6e0af43e80c9d87db90baa155900d
jinyongp <dev.jinyongp@gmail.com>
2022-03-23 16:24:02 +0900

    Feat: TotalScoreBoard 컴포넌트 구현
    - CircleProgress 구현

---
+++ src/components/shared/CircleProgress.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="text-center">
+    <h3
+      v-if="title"
+      class="mb-3 text-sm"
+    >
+      {{ title }}
+    </h3>
+    <div class="relative">
+      <svg
+        :width="radius * 2"
+        :height="radius * 2"
+        overflow="visible"
+      >
+        <circle
+          :cx="radius"
+          :cy="radius"
+          fill="transparent"
+        />
+        <circle
+          :cx="radius"
+          :cy="radius"
+          :r="radius - stroke"
+          fill="transparent"
+          stroke="#ddd"
+          :stroke-width="stroke"
+        />
+        <circle
+          id="circle-progress"
+          :cx="radius"
+          :cy="radius"
+          :r="radius - stroke"
+          fill="transparent"
+          :stroke="color"
+          :stroke-width="stroke"
+          class="origin-center -rotate-90"
+          :stroke-dasharray="circumference"
+          :stroke-dashoffset="progress"
+        />
+        <text
+          :x="radius"
+          :y="radius"
+          text-anchor="middle"
+          dominant-baseline="middle"
+          :fill="color"
+          :font-size="radius / 2.2"
+        >{{ value }}%</text>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  title: {
+    type: String,
+    default: 'Title',
+  },
+  radius: {
+    type: Number,
+    default: 50,
+  },
+  stroke: {
+    type: Number,
+    default: 10,
+  },
+  color: {
+    type: String,
+    default: 'black',
+  },
+  value: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const circumference = 2 * Math.PI * (props.radius - props.stroke);
+const progress = circumference * (1 - (props.value / 100));
+console.log(progress);
+</script>
+
+<style scoped>
+#circle-progress {
+  animation: circle 1s ease;
+}
+
+@keyframes circle {
+  from {
+    stroke-dashoffset: v-bind(circumference) ;
+  }
+  to {
+    stroke-dashoffset: v-bind(progress)
+  }
+}
+
+</style>
---
+++ src/components/user/TotalScoreBoard.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="px-2 bg-white border border-gray-200">
+    <header class="flex items-center justify-between p-1 m-1 border-b">
+      <h5 class="my-1 font-bold">
+        <span class="text-main">종합</span> 전적
+      </h5>
+      <div class="flex items-center gap-1 text-xs">
+        <span>200전</span>
+        <span>135승</span>
+        <span>65패</span>
+      </div>
+    </header>
+    <div class="flex">
+      <circle-progress
+        v-for="(progress, index) in progressList"
+        :key="index"
+        :title="progress.title"
+        :value="progress.value"
+        :color="progress.color"
+        :stroke="7"
+        :radius="45"
+        class="px-2 border-r border-gray-200 last:border-r-0"
+      />
+    </div>
+    <div class="flex items-center justify-between gap-1 p-2 mt-5 border-t">
+      <span class="text-sm text-main">최대주행</span>
+      <span class="text-sm">모드</span>
+      <h4 class="ml-auto text-lg font-bold">
+        통합
+      </h4>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import CircleProgress from '@/components/shared/CircleProgress.vue';
+
+const progressList = [{
+  title: '승률',
+  color: '#07f',
+  value: 68,
+}, {
+  title: '완주율',
+  color: '#9bd728',
+  value: 99,
+}, {
+  title: '리타이어율',
+  color: '#f62459',
+  value: 1,
+},
+];
+</script>
