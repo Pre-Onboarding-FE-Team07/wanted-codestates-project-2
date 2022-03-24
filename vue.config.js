3822f6df34b5110bd8667fb8c98e0e1b46d0b69b
jinyongp <dev.jinyongp@gmail.com>
2022-03-25 05:04:21 +0900

    Deploy: Netlify 배포 설정

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
+  productionSourceMap: false,
   pages: {
     index: {
       entry: './src/main.ts',
       template: './public/index.html',
       title: '카트라이더 전적 검색 TMI',
     },
+  },
+  chainWebpack(config) {
+    config
+      .optimization
+      .minimize(false);
   },
   devServer: {
     proxy: {
