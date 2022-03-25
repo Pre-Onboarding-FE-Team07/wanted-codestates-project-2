2606e7b734f6d1a1035518b5ccd103b2485b5416
jinyongp <dev.jinyongp@gmail.com>
2022-03-25 13:51:46 +0900

    Feat: RecordColumn 연결

--- src/components/user/RecordColumn.vue
+++ src/components/user/RecordColumn.vue
@@ -3,19 +3,34 @@
     class="flex flex-col w-full gap-2 whitespace-nowrap"
   >
     <li
-      v-for="(record, index) in records"
-      :key="index"
+      v-for="record in records"
+      :key="record.matchId"
       class="flex flex-col"
     >
-      <div class="flex items-center justify-between py-3 overflow-x-auto text-sm border-l-4 border-main bg-sky-50 xl:text-base">
-        <span class="mx-3">{{ record.time }}</span>
-        <span class="before:content-['#'] text-main italic text-2xl xl:text-4xl">{{ record.rank }}</span>
-        <span class="before:content-['/'] text-main italic text-base xl:text-xl self-end">{{ record.participants }}</span>
-        <span class="mx-5 xl:mx-12 grow">{{ record.track }}</span>
-        <span class="mx-2 xl:mx-4">{{ record.kart }}</span>
-        <span class="mx-1 xl:mx-3">{{ record.lap }}</span>
+      <div
+        class="flex items-center justify-between py-3 overflow-x-auto text-sm border-l-4 border-r border-y xl:text-base"
+        :class="record.win
+          ? 'bg-sky-50 border-sky-200 border-l-main'
+          : record.retire ? 'bg-red-50 border-red-200 border-l-red-600' : 'bg-white border-gray-300'"
+      >
+        <span class="mx-3 text-xs">{{ record.diffTime }}</span>
+        <div
+          class="min-w-[120px]"
+          :class="record.win ? 'text-main' : record.retire ? 'text-red-500' : 'text-gray-500'"
+        >
+          <span
+            class="before:content-['#']  italic text-2xl xl:text-3xl"
+          >{{ record.retire ? '리타이어' : record.rank }}</span>
+          <span
+            v-if="!record.retire"
+            class="before:content-['/'] italic text-base xl:text-xl self-end"
+          >{{ record.playerCount }}</span>
+        </div>
+        <span class="mx-5 xl:mx-12 grow">{{ record.track.trackName }}</span>
+        <span class="mx-2 xl:mx-4">{{ record.kart.kartName }}</span>
+        <span class="text-center mx-1 xl:mx-3 min-w-[50px]">{{ record.matchTime }}</span>
         <button class="flex items-center justify-center h-full px-5 border-l">
-          <span class="border-[6px] border-transparent border-t-gray-500" />
+          <span class="border-[6px] border-transparent border-t-gray-500 mt-1" />
         </button>
       </div>
     </li>
@@ -32,10 +47,27 @@ export type RankRecord = {
   lap: string;
 };
 
-const records: RankRecord[] = Array(50).fill(
-  {
-    time: '11시간 전', rank: 1, participants: 8, track: '차이나 서안 병마용', kart: '마라톤 V1', lap: "1'45'45",
-  },
-);
+export type MatchRecord = {
+  matchId: string;
+  playerCount: number;
+  rank: number;
+  matchTime: string;
+  kart: {
+    hash: string;
+    kartName: string;
+    kartImg: string;
+  };
+  track: {
+    hash: string;
+    trackName: string;
+    trackImg: string;
+  };
+  win: boolean;
+  retire: boolean;
+  diffTime: string;
+}
 
+defineProps<{
+  records: MatchRecord[];
+}>();
 </script>
