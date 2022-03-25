bd358992081dfb4126bfec527609fb24bb6707b1
jinyongp <dev.jinyongp@gmail.com>
2022-03-25 13:51:36 +0900

    Feat: RankChartBoard 연결

--- src/components/user/RankChartBoard.vue
+++ src/components/user/RankChartBoard.vue
@@ -4,10 +4,10 @@
       <span class="text-main">순위변동</span> 추이
     </template>
     <template #sub-header>
-      <span>지난 200경기</span>
-      <span class="text-main">1.58위</span>
-      <span>최근 50경기</span>
-      <span class="text-main">1.6위</span>
+      <span>지난 {{ rank.last.game }}경기</span>
+      <span class="text-main">{{ rank.last.rate }}위</span>
+      <span>최근 {{ rank.recent.game }}경기</span>
+      <span class="text-main">{{ rank.recent.rate }}위</span>
     </template>
     <template #content>
       <canvas
@@ -24,19 +24,37 @@ import { colors } from '@/styles/theme';
 import {
   Chart, ChartData, ChartOptions, registerables,
 } from 'chart.js';
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
+
+export type RankType = {
+  data: number[];
+  last: {
+    game: number;
+    rate: number;
+  };
+  recent: {
+    game: number;
+    rate: number;
+  }
+};
 
 Chart.register(...registerables);
 
 const canvasRef = ref();
 
 const props = defineProps<{
-  labels: string[];
-  data: number[];
+  rank: RankType;
 }>();
 
+const labels = computed(() => Array.from(
+  { length: props.rank.data.length },
+  (_, i) => `이전 ${i + 1}경기`,
+));
+
+console.log(labels.value);
+
 const data: ChartData<'line'> = {
-  labels: props.labels,
+  labels: labels.value.slice().reverse(),
   datasets: [
     {
       fill: false,
@@ -44,7 +62,7 @@ const data: ChartData<'line'> = {
       borderWidth: 1,
       pointBackgroundColor: colors.main,
       pointRadius: 2,
-      data: props.data,
+      data: props.rank.data.slice().reverse(),
       tension: 0.2,
     },
   ],
