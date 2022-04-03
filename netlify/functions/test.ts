4fb6181557e51087aad173af31bac034f57a1683
jinyongp <dev.jinyongp@gmail.com>
2022-04-03 09:01:03 +0900

    Chore: netlify functions 설정

---
+++ netlify/functions/test.ts
@@ -0,0 +1,6 @@
+import { Handler } from '@netlify/functions';
+
+export const handler: Handler = async (event, context) => ({
+  statusCode: 200,
+  body: JSON.stringify({ message: 'Hello, World!' }),
+});
