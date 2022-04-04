3f9756201fecc8bc420b3b4a75a1089b603d54f3
jinyongp <dev.jinyongp@gmail.com>
2022-04-05 05:56:04 +0900

    Feat: get-match-details function 추가

---
+++ netlify/functions/get-match-details.ts
@@ -0,0 +1,51 @@
+import { API_URL } from '@/netlify/constants/api';
+import { MatchDetailInfo, MatchDetailsDTO } from '@/netlify/types/api';
+import axiosInstance from '@/netlify/utils/axios';
+import { protectHandler } from '@/netlify/utils/error-handler';
+import { getKartByHash } from '@/netlify/utils/rosource';
+import { getLapTime } from '@/netlify/utils/time';
+import { Handler } from '@netlify/functions';
+import { AxiosResponse } from 'axios';
+
+type Query = {
+  matchId?: string;
+}
+
+export const handler: Handler = protectHandler(async (event) => {
+  const { matchId }: Query = event.queryStringParameters || {};
+
+  if (!matchId) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: 'Match not found' }),
+    };
+  }
+
+  const url = API_URL.GET_MATCH_INFO_BY_MATCH_ID(matchId);
+
+  const { status, data }: AxiosResponse<MatchDetailsDTO> = await axiosInstance.get(url);
+
+  const matchDetailInfo: MatchDetailInfo[] = [];
+
+  if (data.players) {
+    const { players } = data;
+    players.forEach((player) => {
+      const retire = player.matchRetired === '1';
+      matchDetailInfo.push({
+        userId: player.accountNo,
+        username: player.characterName,
+        kart: getKartByHash(player.kart),
+        lapTime: retire ? '-' : getLapTime(+player.matchTime),
+        rank: retire ? 8 : +player.matchRank,
+        retire,
+      });
+    });
+  }
+
+  matchDetailInfo.sort((a, b) => +a.rank - +b.rank);
+
+  return {
+    statusCode: status,
+    body: JSON.stringify(matchDetailInfo),
+  };
+});
