3c6cfc6e769aaa5c5094e8740482723eddb88578
jinyongp <dev.jinyongp@gmail.com>
2022-04-05 07:11:07 +0900

    Fix: 불필요한 코드 삭제

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,4 @@
 
 <script lang="ts" setup>
 import HeaderArea from '@/components/shared/HeaderArea.vue';
-import TabNavigation from '@/components/shared/TabNavigation.vue';
-
-const tabs = [
-  { to: '/', name: '홈' },
-  { to: '/rank', name: '랭킹' },
-  { to: '/track', name: '트랙' },
-];
 </script>
