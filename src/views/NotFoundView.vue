47584735cc087d298b1087432b1b3bc46e1c295d
jinyongp <dev.jinyongp@gmail.com>
2022-04-05 17:41:48 +0900

    Feat: NotFoundPage 구현

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>404 Not Found</h1>
+  <tab-navigation
+    bg-color="main"
+    :start-index="-1"
+  />
+  <section class="h-[80vh] flex justify-center items-center flex-col gap-8 text-gray-500">
+    <img
+      src="@/assets/img_404.png"
+      alt="404"
+      class="animate-shake w-[250px] md:w-[300px]"
+    >
+    <h1 class="text-3xl md:text-5xl font-bold">
+      페이지를 찾을 수 없습니다.
+    </h1>
+    <h3 class="text-xs md:text-base">
+      요청하신 페이지가 사라졌거나, 잘못된 경로로 접근했습니다.
+    </h3>
+  </section>
 </template>
+
+<script lang="ts" setup>
+import TabNavigation from '@/components/shared/TabNavigation.vue';
+</script>
